768759923097c7d108886fd72a34db6b0352ddce
kaneko <r202003455of@jindai.jp>
2022-01-28 21:35:37 +0900

    version 3(whileloop)

--- ex5/src/ex5/Hello.java
+++ ex5/src/ex5/Hello.java
@@ -3,8 +3,11 @@ package ex5;
 public class Hello {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++)
+		int i = 0;
+		while (i < 5) {
 			System.out.println("Hello World");
+			i++;
+		}
 
 	}
 
