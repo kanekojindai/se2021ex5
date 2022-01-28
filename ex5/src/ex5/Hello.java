2458e55fa18702a707861b217d2f1eae6d5bb359
kaneko <r202003455of@jindai.jp>
2022-01-28 21:33:56 +0900

    version 2(forloop).

--- ex5/src/ex5/Hello.java
+++ ex5/src/ex5/Hello.java
@@ -3,7 +3,8 @@ package ex5;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		for (int i = 0; i < 5; i++)
+			System.out.println("Hello World");
 
 	}
 
